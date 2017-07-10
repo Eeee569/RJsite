11ee1fd27758a24957fd4e8f8375e1742ae2bec1
Eeee569 <willwolfe1@gmail.com>
2017-07-10 17:33:35 -0400

    added virtualenv and django

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/will/ddb71bc5-8ce9-406e-9f23-65a018b1450b/Projects/RJsite/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
