a611ba2248f429a49faa15d8e3403878e61898de
Eeee569 <willwolfe1@gmail.com>
2017-10-03 21:49:48 -0400

    javascript basics done. Blank django app

--- html/js/main.js
+++ html/js/main.js
@@ -1,13 +1,26 @@
+
+//populate the page with the menue items and other static content
+var menueList = document.getElementsByClassName('portal-sidebar-items')[0].childNodes;
 var request = new XMLHttpRequest();
 request.open('GET','https://learnwebcode.github.io/json-example/animals-1.json');
 request.onload = function() {
 	var menueListData = JSON.parse(request.responseText);
 	console.log(menueListData[0]);
-	var ul = document.getElementsByClassName("portal-sidebar-items");
+	var ul = document.getElementsByClassName('portal-sidebar-items')[0];
 	for(var i =0; i<menueListData.length;i++){
-		var li = document.createElement("li");
+		var li = document.createElement('li');
 		li.appendChild(document.createTextNode(menueListData[i].name));
+		li.addEventListener("click",function() { menueClick(li); } ,false);// = menueClick();
 		ul.appendChild(li);
 	}
+	menueList = document.getElementsByClassName('portal-sidebar-items')[0].childNodes;
 };
 request.send();
+
+//on click request the corisponding item for the page body
+function menueClick(menueItem){
+	console.log("clicked");
+	var mainIframe = document.getElementsByClassName('portal-content-frame')[0];
+	mainIframe.src = 'https://math.dartmouth.edu';
+
+}
